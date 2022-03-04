5735c793adac44a46aa7792ac5c3b31d57b25c27
Md (Raiyan) Hassan <70424287+rhassan19996@users.noreply.github.com>
2022-03-04 14:02:54 -0500

    Update SuperheroDetailsViewController.swift

--- MoviesApp/Controller/SuperheroDetailsViewController.swift
+++ MoviesApp/Controller/SuperheroDetailsViewController.swift
@@ -1,10 +1,6 @@
 //
 //  SuperheroDetailsViewController.swift
-//  Flixter
-//
-//  Created by Joy Paul on 2/23/19.
-//  Copyright © 2019 Joy Paul. All rights reserved.
-//
+
 
 import UIKit
 import AlamofireImage
